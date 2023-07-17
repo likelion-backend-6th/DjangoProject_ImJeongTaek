6f111119bd04528e1453ab0eb468784cf6e7089e
Jeong Taek <imjt9345@naver.com>
2023-07-17 15:41:48 +0900

    라인 자동 정리

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from blog.models import Post
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
