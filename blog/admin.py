cd88755e105d038265e4785ef56ce01c7614bf4d
Jeong Taek <imjt9345@naver.com>
2023-07-17 15:38:58 +0900

    blog의 Post 모델 어드민 데코레이션으로 변경

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from blog.models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
