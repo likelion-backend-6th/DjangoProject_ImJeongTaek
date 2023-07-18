0e76fad18659a8b1af789725220482d9723f17ee
Jeong Taek <imjt9345@naver.com>
2023-07-18 15:17:33 +0900

    모델 get_absolute_url 추가, 템플릿 수정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
@@ -41,3 +42,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
