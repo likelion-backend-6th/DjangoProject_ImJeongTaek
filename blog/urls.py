9cccbf5726f09aa02fdbe9972d8f9ab8b10973f4
Jeong Taek <imjt9345@naver.com>
2023-07-19 15:34:02 +0900

    post_share 뷰 로직 수정, url 추가

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('list/', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<post>[-\w]+)/$', views.post_detail,
-            name='post_detail'),]
+            name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+]
